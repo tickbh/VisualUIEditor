3dc61470e919e4b17bf65f6ee54693bbe9f15ada
tickbh <tickdream125@hotmail.com>
2017-02-06 09:48:45 +0800

    移除无用的日志

--- ext/ExtArmature.js
+++ ext/ExtArmature.js
@@ -4,57 +4,53 @@ ExtArmature.name = 'UIArmature'
 ExtArmature.icon = 'app://res/control/Scale9.png'
 ExtArmature.tag = 10
 
-ExtArmature.GenEmptyNode = function () {
-  node = new ccs.Armature()
-  node._className = ExtArmature.name
-  return node
-}
-
-ExtArmature.SetBaseNodeProp = function (node, data) {
-  data.actionName && (node.actionName = data.actionName)
-  data.imagePath && (node.imagePath = data.imagePath)
-  data.plistPath && (node.plistPath = data.plistPath)
-  data.configFilePath && (node.configFilePath = data.configFilePath)
-}
-
-ExtArmature.GenNodeByData = function (data, parent) {
-  // data.actionName = 'skill1'
-  // data.imagePath = 'fla/skill1.png'
-  // data.plistPath = 'fla/skill1.plist'
-  // data.configFilePath = 'fla/skill1.xml'
-
-  node = new ccs.Armature()
-  node._className = ExtArmature.name
-  ExtArmature.SetBaseNodeProp(node, data)
-  // data.ignoreSetProp = true
-  if (data.uuid) {
-    node.uuid = data.uuid
-  }
-  var fullImagePath = getFullPathForName(data.imagePath)
-  var fullPlistPath = getFullPathForName(data.plistPath)
-  var fullConfigPath = getFullPathForName(data.configFilePath)
-  if (!fullImagePath || !fullPlistPath || !fullConfigPath || !data.actionName) {
+ExtArmature.GenEmptyNode = function() {
+    node = new ccs.Armature()
+    node._className = ExtArmature.name
     return node
-  }
-  // cc.loader.load([fullImagePath, fullPlistPath, fullConfigPath],
-  //   function (results, count, loadedCount) {
-  //     if (count == loadedCount + 1) {
-  //     }
-  //   }, function () {
-      ccs.armatureDataManager.addArmatureFileInfo(fullImagePath, fullPlistPath, fullConfigPath)
-      // if(!node.getParent() && parent) {
-      //   parent.addChild(node)
-      // }
-      node.init(data.actionName)
-      if (node.getAnimation()) {
-        node.getAnimation().play('stand', -1, 1)
-      }
+}
 
-      console.log("parent111 = ", parent)
-      console.log("parent = ", node.getParent())
-      console.log("getNodeToParentTransform = ", node.getNodeToParentTransform())
-      console.log("_offsetPoint = ", node._offsetPoint)
-      // cocosGenNodeByDataBase(data, node, parent)
+ExtArmature.SetBaseNodeProp = function(node, data) {
+    data.actionName && (node.actionName = data.actionName)
+    data.imagePath && (node.imagePath = data.imagePath)
+    data.plistPath && (node.plistPath = data.plistPath)
+    data.configFilePath && (node.configFilePath = data.configFilePath)
+}
+
+ExtArmature.GenNodeByData = function(data, parent) {
+    // data.actionName = 'skill1'
+    // data.imagePath = 'fla/skill1.png'
+    // data.plistPath = 'fla/skill1.plist'
+    // data.configFilePath = 'fla/skill1.xml'
+
+    node = new ccs.Armature()
+    node._className = ExtArmature.name
+    ExtArmature.SetBaseNodeProp(node, data)
+        // data.ignoreSetProp = true
+    if (data.uuid) {
+        node.uuid = data.uuid
+    }
+    var fullImagePath = getFullPathForName(data.imagePath)
+    var fullPlistPath = getFullPathForName(data.plistPath)
+    var fullConfigPath = getFullPathForName(data.configFilePath)
+    if (!fullImagePath || !fullPlistPath || !fullConfigPath || !data.actionName) {
+        return node
+    }
+    // cc.loader.load([fullImagePath, fullPlistPath, fullConfigPath],
+    //   function (results, count, loadedCount) {
+    //     if (count == loadedCount + 1) {
+    //     }
+    //   }, function () {
+    ccs.armatureDataManager.addArmatureFileInfo(fullImagePath, fullPlistPath, fullConfigPath)
+        // if(!node.getParent() && parent) {
+        //   parent.addChild(node)
+        // }
+    node.init(data.actionName)
+    if (node.getAnimation()) {
+        node.getAnimation().play('stand', -1, 1)
+    }
+
+    // cocosGenNodeByDataBase(data, node, parent)
     // var newNode = new ccs.Armature(data.actionName)
     // newNode._className = ExtArmature.name
     // node.uuid = data.uuid || gen_uuid()
@@ -65,131 +61,127 @@ ExtArmature.GenNodeByData = function (data, parent) {
     // 
     // ExtArmature.SetBaseNodeProp(newNode, data)
     // })
-  return node
+    return node
 }
 
-ExtArmature.ResetPropByData = function (control, data, parent) {
-  if (data.ignoreSetProp) {
-    return
-  }
-  let node = control._node
-  parent = parent || node.getParent()
+ExtArmature.ResetPropByData = function(control, data, parent) {
+    if (data.ignoreSetProp) {
+        return
+    }
+    let node = control._node
+    parent = parent || node.getParent()
 
-  data.ignoreSetProp = true
-  data.uuid = node.uuid
-  let newNode = cocosGenNodeByData(data, parent)
-  node.ignoreAddToParent = true
-  ReplaceNode(node, newNode, parent)
-  control._node = newNode
+    data.ignoreSetProp = true
+    data.uuid = node.uuid
+    let newNode = cocosGenNodeByData(data, parent)
+    node.ignoreAddToParent = true
+    ReplaceNode(node, newNode, parent)
+    control._node = newNode
 }
 
-ExtArmature.SetNodePropByData = function (node, data, parent) {
-  ExtArmature.ResetPropByData({ _node: node }, data, parent)
+ExtArmature.SetNodePropByData = function(node, data, parent) {
+    ExtArmature.ResetPropByData({ _node: node }, data, parent)
 }
 
-ExtArmature.ExportNodeData = function (node, data) {
-  node.actionName && (data.actionName = node.actionName)
-  node.imagePath && (data.imagePath = node.imagePath)
-  node.plistPath && (data.plistPath = node.plistPath)
-  node.configFilePath && (data.configFilePath = node.configFilePath)
+ExtArmature.ExportNodeData = function(node, data) {
+    node.actionName && (data.actionName = node.actionName)
+    node.imagePath && (data.imagePath = node.imagePath)
+    node.plistPath && (data.plistPath = node.plistPath)
+    node.configFilePath && (data.configFilePath = node.configFilePath)
 
-  data['anchorX'] = node.anchorX  
-  data['anchorY'] = node.anchorY
+    data['anchorX'] = node.anchorX
+    data['anchorY'] = node.anchorY
 
-  // data['anchorX'] = null
-  // data['anchorY'] = null
+    // data['anchorX'] = null
+    // data['anchorY'] = null
 }
 
-ExtArmature.PropCantChange = function (node, path, value, target) {
-  // if(path == "anchor.x" || path == "anchor.y") {
-  //   return true
-  // }
-  return false
+ExtArmature.PropCantChange = function(node, path, value, target) {
+    // if(path == "anchor.x" || path == "anchor.y") {
+    //   return true
+    // }
+    return false
 }
 
-ExtArmature.SetPropChange = function (control, path, value, target) {
-  let data = cocosExportNodeData(control._node, { uuid: true })
-  data[path] = value
-  ExtArmature.ResetPropByData(control, data)
+ExtArmature.SetPropChange = function(control, path, value, target) {
+    let data = cocosExportNodeData(control._node, { uuid: true })
+    data[path] = value
+    ExtArmature.ResetPropByData(control, data)
 }
 
 ExtArmature.GetLoadImages = function(data) {
-  return [data['imagePath'], data['plistPath'], data['configFilePath']]
-} 
-
-ExtArmature.NodifyPropChange = function (control) {
-  SetNodifyPropChange(control)
+    return [data['imagePath'], data['plistPath'], data['configFilePath']]
 }
 
-function ExtArmatureData (node) {
-  this._node = node
+ExtArmature.NodifyPropChange = function(control) {
+    SetNodifyPropChange(control)
+}
+
+function ExtArmatureData(node) {
+    this._node = node
 }
 
 ExtArmatureData.prototype = {
-  __displayName__: 'ExtArmature',
-  __type__: 'ccui.ExtArmature',
+    __displayName__: 'ExtArmature',
+    __type__: 'ccui.ExtArmature',
 
-  get actionName() {
-    return {
-      path: 'actionName',
-      type: 'asset',
-      name: 'actionName',
-      attrs: {
-      },
-      value: this._node.actionName
+    get actionName() {
+        return {
+            path: 'actionName',
+            type: 'asset',
+            name: 'actionName',
+            attrs: {},
+            value: this._node.actionName
+        }
+    },
+
+    get imagePath() {
+        return {
+            path: 'imagePath',
+            type: 'asset',
+            name: 'imagePath',
+            attrs: {},
+            value: this._node.imagePath
+        }
+    },
+
+    get plistPath() {
+        return {
+            path: 'plistPath',
+            type: 'asset',
+            name: 'plistPath',
+            attrs: {},
+            value: this._node.plistPath
+        }
+    },
+
+    get configFilePath() {
+        return {
+            path: 'configFilePath',
+            type: 'asset',
+            name: 'configFilePath',
+            attrs: {},
+            value: this._node.configFilePath
+        }
+    },
+
+    get __props__() {
+        return [
+            this.actionName,
+            this.imagePath,
+            this.plistPath,
+            this.configFilePath
+        ]
     }
-  },
-
-  get imagePath() {
-    return {
-      path: 'imagePath',
-      type: 'asset',
-      name: 'imagePath',
-      attrs: {
-      },
-      value: this._node.imagePath
-    }
-  },
-
-  get plistPath() {
-    return {
-      path: 'plistPath',
-      type: 'asset',
-      name: 'plistPath',
-      attrs: {
-      },
-      value: this._node.plistPath
-    }
-  },
-
-  get configFilePath() {
-    return {
-      path: 'configFilePath',
-      type: 'asset',
-      name: 'configFilePath',
-      attrs: {
-      },
-      value: this._node.configFilePath
-    }
-  },
-
-  get __props__() {
-    return [
-      this.actionName,
-      this.imagePath,
-      this.plistPath,
-      this.configFilePath
-    ]
-  }
 }
 
 ExtArmature.ExtArmatureData = ExtArmatureData
 
-ExtArmature.PropComps = function (node) {
-  let datas = [new WidgetData(node)]
-  datas.push(new TouchData(node))
-  datas.push(new ExtArmatureData(node))
-  return datas
+ExtArmature.PropComps = function(node) {
+    let datas = [new WidgetData(node)]
+    datas.push(new TouchData(node))
+    datas.push(new ExtArmatureData(node))
+    return datas
 }
 
 module.exports = ExtArmature
--- js/UIUtil.js
+++ js/UIUtil.js
@@ -4,257 +4,256 @@ let projectLangData = null
 let currentLangSet = null
 let projectLangReadTime = 0
 
-function ChangeProjectFolder () {
-  let newFolder = Electron.remote.dialog.showOpenDialog({properties: ['openFile', 'openDirectory']})
-  if (newFolder) {
-    window.projectFolder = newFolder[0]
-    window.localStorage['projectFolder'] = newFolder[0]
-    Ipc.sendToAll('ui:project_floder_change', {folder: newFolder[0]})
-  }
+function ChangeProjectFolder() {
+    let newFolder = Electron.remote.dialog.showOpenDialog({ properties: ['openFile', 'openDirectory'] })
+    if (newFolder) {
+        window.projectFolder = newFolder[0]
+        window.localStorage['projectFolder'] = newFolder[0]
+        Ipc.sendToAll('ui:project_floder_change', { folder: newFolder[0] })
+    }
 }
 
 ipcRenderer.on("ui:new-project", (event, project) => {
     window.projectFolder = project
     window.localStorage['projectFolder'] = project
-    Ipc.sendToAll('ui:project_floder_change', {folder: project})
+    Ipc.sendToAll('ui:project_floder_change', { folder: project })
 })
 
 ipcRenderer.on('ui:open-project-folder', (event, message, ...args) => {
-  ChangeProjectFolder()
+    ChangeProjectFolder()
 })
 
 ipcRenderer.on('ui:new-ui-file', (event, message, ...args) => {
-  Ipc.sendToAllPanel('ui:new-ui-file')
+    Ipc.sendToAllPanel('ui:new-ui-file')
 })
 
 ipcRenderer.on('ui:openPanel', (event, panelName, a) => {
-  console.log(panelName)
-  ChangeProjectFolder()
+    ChangeProjectFolder()
 })
 
-function DealWithRenderMessage (message, ...args) {
-}
+function DealWithRenderMessage(message, ...args) {}
 
-function applyFilterByTree (tree, filter) {
-  filter = (filter || '').toLowerCase()
-  function recursiveFilter (item, filter) {
-    let show = false
-    if (!filter || filter.length == 0) {
-      show = true
-    } else if (item.name.toLowerCase().indexOf(filter) >= 0) {
-      show = true
+function applyFilterByTree(tree, filter) {
+    filter = (filter || '').toLowerCase()
+
+    function recursiveFilter(item, filter) {
+        let show = false
+        if (!filter || filter.length == 0) {
+            show = true
+        } else if (item.name.toLowerCase().indexOf(filter) >= 0) {
+            show = true
+        }
+
+        let children = Polymer.dom(item).children
+        for (var i = 0; i < children.length; i++) {
+            let childShow = recursiveFilter(children[i], filter)
+            show = show || childShow
+        }
+        item.hidden = !show
+        return show
     }
-
-    let children = Polymer.dom(item).children
+    let children = Polymer.dom(tree).children
     for (var i = 0; i < children.length; i++) {
-      let childShow = recursiveFilter(children[i], filter)
-      show = show || childShow
+        recursiveFilter(children[i], filter)
     }
-    item.hidden = !show
-    return show
-  }
-  let children = Polymer.dom(tree).children
-  for (var i = 0; i < children.length; i++) {
-    recursiveFilter(children[i], filter)
-  }
 }
 
-function treeExpandAll (tree) {
-  function recursiveExpand (item) {
-    item.folded = false
-    let children = Polymer.dom(item).children
+function treeExpandAll(tree) {
+    function recursiveExpand(item) {
+        item.folded = false
+        let children = Polymer.dom(item).children
+        for (var i = 0; i < children.length; i++) {
+            recursiveExpand(children[i])
+        }
+    }
+    let children = Polymer.dom(tree).children
     for (var i = 0; i < children.length; i++) {
-      recursiveExpand(children[i])
+        recursiveExpand(children[i])
     }
-  }
-  let children = Polymer.dom(tree).children
-  for (var i = 0; i < children.length; i++) {
-    recursiveExpand(children[i])
-  }
 }
 
-function treeFoldedAll (tree) {
-  function recursiveFolded (item) {
-    item.folded = true
-    let children = Polymer.dom(item).children
+function treeFoldedAll(tree) {
+    function recursiveFolded(item) {
+        item.folded = true
+        let children = Polymer.dom(item).children
+        for (var i = 0; i < children.length; i++) {
+            recursiveFolded(children[i])
+        }
+    }
+    let children = Polymer.dom(tree).children
     for (var i = 0; i < children.length; i++) {
-      recursiveFolded(children[i])
+        recursiveFolded(children[i])
     }
-  }
-  let children = Polymer.dom(tree).children
-  for (var i = 0; i < children.length; i++) {
-    recursiveFolded(children[i])
-  }
 }
 
-function calcRelativePath (parentPath, subPath) {
-  parentPath = parentPath.replace(/\\/g, '/')
-  subPath = subPath.replace(/\\/g, '/')
-  let index = subPath.indexOf(parentPath)
-  if (index < 0) {
-    return subPath
-  }
-  return subPath.substr(index + parentPath.length)
+function calcRelativePath(parentPath, subPath) {
+    parentPath = parentPath.replace(/\\/g, '/')
+    subPath = subPath.replace(/\\/g, '/')
+    let index = subPath.indexOf(parentPath)
+    if (index < 0) {
+        return subPath
+    }
+    return subPath.substr(index + parentPath.length)
 }
 
-function checkTextureExist (url) {
-  if (!url) {
-    return false
-  }
-  var tex = cc.textureCache.getTextureForKey(url)
-  if (!tex) {
-    tex = cc.textureCache.addImage(url)
+function checkTextureExist(url) {
+    if (!url) {
+        return false
+    }
+    var tex = cc.textureCache.getTextureForKey(url)
     if (!tex) {
-      return false
+        tex = cc.textureCache.addImage(url)
+        if (!tex) {
+            return false
+        }
     }
-  }
-  return true
+    return true
 }
 
-function openHtmlInNewWindow (url, width, height) {
-  //   const modalPath = path.join(url)
-  let win = new Electron.remote.BrowserWindow({ width: width || 400, height: height || 320 })
-  win.on('closed', function () { win = null })
-  win.loadURL(url) // 指定渲染的页面
-  win.show() // 打开一个窗口
+function openHtmlInNewWindow(url, width, height) {
+    //   const modalPath = path.join(url)
+    let win = new Electron.remote.BrowserWindow({ width: width || 400, height: height || 320 })
+    win.on('closed', function() { win = null })
+    win.loadURL(url) // 指定渲染的页面
+    win.show() // 打开一个窗口
 }
 
-function ToHumanText (text) {
-  let result = text.replace(/[-_]([a-z])/g, function (m) {
-    return m[1].toUpperCase()
-  })
+function ToHumanText(text) {
+    let result = text.replace(/[-_]([a-z])/g, function(m) {
+        return m[1].toUpperCase()
+    })
 
-  result = result.replace(/([a-z][A-Z])/g, function (g) {
-    return g[0] + ' ' + g[1]
-  })
+    result = result.replace(/([a-z][A-Z])/g, function(g) {
+        return g[0] + ' ' + g[1]
+    })
 
-  if (result.charAt(0) === ' ') {
-    result.slice(1)
-  }
-
-  return result.charAt(0).toUpperCase() + result.slice(1)
-}
-
-function FocusParent (element) {
-  let parent = element.parentElement
-  while ( parent ) {
-    if (
-      parent.tabIndex !== null &&
-      parent.tabIndex !== undefined &&
-      parent.tabIndex !== -1
-    ) {
-      parent.focus()
-      return
+    if (result.charAt(0) === ' ') {
+        result.slice(1)
     }
 
-    parent = parent.parentElement
-  }
+    return result.charAt(0).toUpperCase() + result.slice(1)
 }
 
-function IsSelfOrAncient (element, ancientEL) {
-  let parent = element
-  while ( parent ) {
-    if (parent === ancientEL) {
-      return true
+function FocusParent(element) {
+    let parent = element.parentElement
+    while (parent) {
+        if (
+            parent.tabIndex !== null &&
+            parent.tabIndex !== undefined &&
+            parent.tabIndex !== -1
+        ) {
+            parent.focus()
+            return
+        }
+
+        parent = parent.parentElement
+    }
+}
+
+function IsSelfOrAncient(element, ancientEL) {
+    let parent = element
+    while (parent) {
+        if (parent === ancientEL) {
+            return true
+        }
+
+        parent = Polymer.dom(parent).parentNode
     }
 
-    parent = Polymer.dom(parent).parentNode
-  }
-
-  return false
+    return false
 }
 
-function ArrayCmpFilter (items, func) {
-  let results = []
-  for ( let i = 0; i < items.length; ++i) {
-    let item = items[i]
-    let add = true
+function ArrayCmpFilter(items, func) {
+    let results = []
+    for (let i = 0; i < items.length; ++i) {
+        let item = items[i]
+        let add = true
 
-    for ( let j = 0; j < results.length; ++j) {
-      let addedItem = results[j]
+        for (let j = 0; j < results.length; ++j) {
+            let addedItem = results[j]
 
-      if (item === addedItem) {
-        // existed
-        add = false
-        break
-      }
+            if (item === addedItem) {
+                // existed
+                add = false
+                break
+            }
 
-      let cmp = func(addedItem, item)
-      if (cmp > 0) {
-        add = false
-        break
-      } else if (cmp < 0) {
-        results.splice(j, 1)
-        --j
-      }
+            let cmp = func(addedItem, item)
+            if (cmp > 0) {
+                add = false
+                break
+            } else if (cmp < 0) {
+                results.splice(j, 1)
+                    --j
+            }
+        }
+
+        if (add) {
+            results.push(item)
+        }
     }
 
-    if (add) {
-      results.push(item)
+    return results
+}
+
+function CustomAddListener(elem, type, callback) {
+    if (elem.addEventListener) {
+        return elem.addEventListener(type, callback, false)
     }
-  }
-
-  return results
-}
-
-function CustomAddListener (elem, type, callback) {
-  if (elem.addEventListener) {
-    return elem.addEventListener(type, callback, false)
-  }
-  if (elem.attachEvent) {
-    var wrapper = function () {
-      callback.call(elem, window.event)
+    if (elem.attachEvent) {
+        var wrapper = function() {
+            callback.call(elem, window.event)
+        }
+        callback._wrapper = wrapper
+        elem.attachEvent('on' + type, wrapper)
     }
-    callback._wrapper = wrapper
-    elem.attachEvent('on' + type, wrapper)
-  }
 }
 
-function OpenLangInfo (langPath) {
-  AddOrModifyConfig('currentLangOpenPath', langPath, true)
-  Ipc.sendToMainDirect('ipc-showgrid', window.projectFolder)
+function OpenLangInfo(langPath) {
+    AddOrModifyConfig('currentLangOpenPath', langPath, true)
+    Ipc.sendToMainDirect('ipc-showgrid', window.projectFolder)
 }
 
-function getProjectLangData () {
-  if (!window.projectFolder) {
-    return {}
-  }
-
-  let file = getLangPath() + '/' + getLangFileName()
-  if (!fs.existsSync(file)) {
-    return {}
-  }
-  var stat = fs.statSync(file)
-  if (stat.mtime.getTime() != projectLangReadTime) {
-    projectLangData = null
-  }
-
-  if (projectLangData == null) {
-    projectLangData = GetFileToData(file)
-    currentLangSet = getCurLangSet()
-    projectLangReadTime = stat.mtime.getTime()
-  }
-  return projectLangData
-}
-
-function getLangFromConfig (key) {
-  let data = getProjectLangData()
-  if (data[key] == null) {
-    return null
-  }
-  return projectLangData[key][currentLangSet] || ''
-}
-
-function tryAnalyseLang (value) {
-  let data = {isKey: false}
-  if (value.indexOf('@') == 0) {
-    let key = value.substring(1)
-    let lang = getLangFromConfig(key)
-    if (lang != null) {
-      data.value = lang
-      data.isKey = true
-      data.key = key
+function getProjectLangData() {
+    if (!window.projectFolder) {
+        return {}
     }
-  }
-  return data
+
+    let file = getLangPath() + '/' + getLangFileName()
+    if (!fs.existsSync(file)) {
+        return {}
+    }
+    var stat = fs.statSync(file)
+    if (stat.mtime.getTime() != projectLangReadTime) {
+        projectLangData = null
+    }
+
+    if (projectLangData == null) {
+        projectLangData = GetFileToData(file)
+        currentLangSet = getCurLangSet()
+        projectLangReadTime = stat.mtime.getTime()
+    }
+    return projectLangData
+}
+
+function getLangFromConfig(key) {
+    let data = getProjectLangData()
+    if (data[key] == null) {
+        return null
+    }
+    return projectLangData[key][currentLangSet] || ''
+}
+
+function tryAnalyseLang(value) {
+    let data = { isKey: false }
+    if (value.indexOf('@') == 0) {
+        let key = value.substring(1)
+        let lang = getLangFromConfig(key)
+        if (lang != null) {
+            data.value = lang
+            data.isKey = true
+            data.key = key
+        }
+    }
+    return data
 }
